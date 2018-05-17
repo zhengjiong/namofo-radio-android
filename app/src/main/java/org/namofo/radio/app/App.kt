985edc7c8f2ec4905681039061ba120296d1c893
zhengjiong <253586504@qq.com>
2018-05-18 03:20:25 +0800

    feat(music): add musiclibrary

--- app/src/main/java/org/namofo/radio/app/App.kt
+++ app/src/main/java/org/namofo/radio/app/App.kt
@@ -2,6 +2,11 @@ package org.namofo.radio.app
 
 import com.blankj.utilcode.util.Utils
 import com.jess.arms.base.BaseApplication
+import com.lzx.musiclibrary.cache.CacheConfig
+import com.lzx.musiclibrary.utils.BaseUtil
+import com.lzx.musiclibrary.manager.MusicLibrary
+
+
 
 /**
  * Created by zhengjiong
@@ -19,6 +24,16 @@ class App : BaseApplication() {
         super.onCreate()
         app = this@App
         Utils.init(this)
+        if (!BaseUtil.getCurProcessName(this).contains(":musicLibrary")) {
+            val builder = MusicLibrary.Builder(this)
+                    .setAutoPlayNext(false)//是否在播放完当前歌曲后自动播放下一首
+                    .setUseMediaPlayer(false)//是否使用 MediaPlayer
+                    //.setNotificationCreater(NotificationCreater)//通知栏配置
+                    .setCacheConfig(CacheConfig.DEFAULT)//边播边存配置
+            val musicLibrary = builder
+                    .build()
+            musicLibrary.init()
+        }
     }
 
 
